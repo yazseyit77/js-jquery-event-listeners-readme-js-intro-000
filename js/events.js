fded26f75e10d5abb4904c2af64ae7a8bc0a506a
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-03-07 04:51:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,33 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+
+function getIt(){
+  $("p").on("click", function(){ return alert("Hey!")})
+}
+
+function frameIt(){
+  $("img").on("load", function(){ $("img").addClass("tasty")})
+}
+
+function pressIt(){
+  $("#typing").on("keydown", function(e){
+      if(e.which === 71){
+        alert("G was pressed");
+        return;
+      }
+      return;
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
