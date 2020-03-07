061dd6530ee47351e0470484e10b501f8549b780
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-03-07 04:40:54 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -29,5 +29,10 @@ function pressIt(){
 }
 
 function submitIt(){
-  $("form").on("submit", function(){ $("img").addClass("tasty")})
+  $("form").on("submit", function(){ if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;})
 }
